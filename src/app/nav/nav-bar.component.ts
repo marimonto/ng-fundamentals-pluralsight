7d89c31d702a6c9ef41d33324d4a95d0801ee8ec
Mariana Montoya <marimontod@gmail.com>
2023-01-07 12:21:36 -0500

    Module-6: Routing and Navigating Pages

--- src/app/nav/nav-bar.component.ts
+++ src/app/nav/nav-bar.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
-  styles: [`
-    .nav.navbar-nav {font-size: 15px;}
-    #searchForm {margin-right: 100px;}
-    @media (max-width: 1200px) {#searchForm {display:none}}
-  `]
+  styles: [
+    `
+      .nav.navbar-nav {
+        font-size: 15px;
+      }
+      #searchForm {
+        margin-right: 100px;
+      }
+      @media (max-width: 1200px) {
+        #searchForm {
+          display: none;
+        }
+      }
+      li > a.active {
+        color: #f97924;
+      }
+    `,
+  ],
 })
-export class NavBarComponent {
-
-}
+export class NavBarComponent { }
